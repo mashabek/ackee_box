157ee638e675d8ff270cb539ea7073fd61d5a3c3
Bekhzod Masharipov <bekvevo@gmail.com>
2025-06-23 21:53:54 +0200

    add prisma & init migration

--- prisma/migrations/20250622224420_init/migration.sql
+++ prisma/migrations/20250622224420_init/migration.sql
@@ -1,11 +1,13 @@
+-- Enable PostGIS extension
+CREATE EXTENSION IF NOT EXISTS postgis;
+
 -- CreateTable
 CREATE TABLE "Box" (
     "id" TEXT NOT NULL,
     "code" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "address" TEXT NOT NULL,
-    "lat" DOUBLE PRECISION NOT NULL,
-    "lng" DOUBLE PRECISION NOT NULL,
+    "location" geography(Point, 4326) NOT NULL,
     "status" TEXT,
 
     CONSTRAINT "Box_pkey" PRIMARY KEY ("id")
@@ -13,3 +15,6 @@ CREATE TABLE "Box" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Box_code_key" ON "Box"("code");
+
+-- Create spatial index
+CREATE INDEX "Box_location_gix" ON "Box" USING GIST ("location");
